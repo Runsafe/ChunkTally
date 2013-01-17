0d6e2f564a1eb20d1241d06c45935a2931da7326
Morten Nilsen <morten@runsafe.no>
2013-01-17 20:24:56 +0100

    Make it possible to have different limits for different entities - Add detailed coordinates to output

--- src/no/runsafe/chunktally/Accountant.java
+++ src/no/runsafe/chunktally/Accountant.java
@@ -4,6 +4,7 @@ import no.runsafe.framework.configuration.IConfiguration;
 import no.runsafe.framework.event.IConfigurationChanged;
 import no.runsafe.framework.event.world.IChunkLoad;
 import no.runsafe.framework.output.IOutput;
+import no.runsafe.framework.server.RunsafeLocation;
 import no.runsafe.framework.server.chunk.RunsafeChunk;
 import no.runsafe.framework.server.entity.RunsafeEntity;
 
@@ -20,12 +21,22 @@ public class Accountant implements IChunkLoad, IConfigurationChanged
 	@Override
 	public void OnChunkLoad(RunsafeChunk chunk)
 	{
-		if (chunk.getEntities().size() > chunkEntityAudit)
+		if (chunk.getEntities().size() > auditLevel)
 		{
 			if (!auditedWorlds.contains(chunk.getWorld().getName()))
 				return;
 			AuditEntitiesAboveLimit(chunk);
 		}
+	}
+
+	@Override
+	public void OnConfigurationChanged(IConfiguration config)
+	{
+		limits.clear();
+		limits.putAll(config.getConfigValuesAsIntegerMap("audit.entity.limit"));
+		auditedWorlds.clear();
+		auditedWorlds.addAll(config.getConfigValueAsList("audit.entity.worlds"));
+		auditLevel = config.getConfigValueAsInt("audit.entity.inspect");
 	}
 
 	private void AuditEntitiesAboveLimit(RunsafeChunk chunk)
@@ -34,14 +45,33 @@ public class Accountant implements IChunkLoad, IConfigurationChanged
 		if (entities == null)
 			return;
 		HashMap<String, Integer> counts = new HashMap<String, Integer>();
+		HashMap<String, RunsafeLocation> locations = new HashMap<String, RunsafeLocation>();
 		for (RunsafeEntity entity : entities)
 		{
-			String name = entity.getRaw().getClass().getSimpleName();
+			String name = entity.getRaw().getType().name();
+			if (!locations.containsKey(name))
+				locations.put(name, entity.getLocation());
 			if (!counts.containsKey(name))
 				counts.put(name, 0);
 			else
 				counts.put(name, counts.get(name) + 1);
 		}
+		for (String type : counts.keySet())
+		{
+			if (limits.containsKey(type))
+			{
+				if (counts.get(type) < limits.get(type))
+					counts.remove(type);
+			}
+			else if (limits.containsKey("default") && counts.get(type) < limits.get("default"))
+				counts.remove(type);
+		}
+		int finalCount = 0;
+		for (String type : counts.keySet())
+			finalCount += counts.get(type);
+		if (finalCount < auditLevel)
+			return;
+
 		ArrayList<Map.Entry<String, Integer>> as = new ArrayList<Map.Entry<String, Integer>>(counts.entrySet());
 		Collections.sort(as, new Comparator<Map.Entry<String, Integer>>()
 		{
@@ -51,28 +81,32 @@ public class Accountant implements IChunkLoad, IConfigurationChanged
 			}
 		});
 		Map.Entry<String, Integer> max = as.get(0);
+		StringBuilder stats = new StringBuilder();
+		for (String type : counts.keySet())
+			stats.append(
+				String.format(
+					"  %d x %s (%d,%d,%d)",
+					counts.get(type), type,
+					locations.get(type).getBlockX(),
+					locations.get(type).getBlockY(),
+					locations.get(type).getBlockZ()
+				)
+			);
 		console.writeColoured(
-			"&cChunk [%s,%d,%d] is above entity limit! %d > %d (%d %s)&r",
+			"&cChunk [%s,%d,%d] is above entity limit! %d > %d&r\n%s",
 			Level.WARNING,
 			chunk.getWorld().getName(),
 			chunk.getX(),
 			chunk.getZ(),
 			entities.size(),
-			chunkEntityAudit,
+			auditLevel,
 			max.getValue(),
 			max.getKey()
 		);
 	}
 
-	@Override
-	public void OnConfigurationChanged(IConfiguration config)
-	{
-		chunkEntityAudit = config.getConfigValueAsInt("audit.entity.limit");
-		auditedWorlds.clear();
-		auditedWorlds.addAll(config.getConfigValueAsList("audit.entity.worlds"));
-	}
-
 	private final IOutput console;
 	private final ArrayList<String> auditedWorlds = new ArrayList<String>();
-	private int chunkEntityAudit;
+	private final HashMap<String, Integer> limits = new HashMap<String, Integer>();
+	private int auditLevel;
 }
