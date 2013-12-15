4730de24cc56c9414d5069186d43dcefd1062074
Morten Nilsen <morten@runsafe.no>
2013-12-15 12:14:26 +0100

    Declare features used

--- src/no/runsafe/chunktally/Plugin.java
+++ src/no/runsafe/chunktally/Plugin.java
@@ -1,12 +1,14 @@
 package no.runsafe.chunktally;
 
 import no.runsafe.framework.RunsafeConfigurablePlugin;
+import no.runsafe.framework.features.Events;
 
 public class Plugin extends RunsafeConfigurablePlugin
 {
 	@Override
 	protected void PluginSetup()
 	{
+		addComponent(Events.class);
 		addComponent(Accountant.class);
 	}
 }
